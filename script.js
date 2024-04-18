2068aae03665e82c26bde6eea28f01594f5e1112
saminulldreinull <110830300+saminulldreinull@users.noreply.github.com>
2024-04-18 23:25:34 +0200

    header

--- script.js
+++ script.js
@@ -21,13 +21,26 @@ window.addEventListener('scroll', function() {
     var section = document.getElementById('facts-section'); // Ersetzen Sie 'sectionId' mit der tatsächlichen ID Ihrer Section.
     var sectionTop = section.offsetTop; // Top-Position der Section relativ zum Dokument.
     var sectionHeight = section.offsetHeight; // Höhe der Section.
+    var logo = document.getElementById('logo');
+    var logoContainer = document.getElementById('logo-container');
+
 
     if (window.scrollY >= sectionTop && window.scrollY < sectionTop + sectionHeight) {
         header.style.backgroundColor = 'rgb(0,0,77)'; // Ersetzen Sie 'neueFarbe' mit der gewünschten Farbe.
+        logo.classList.add('logo-shrinked')
+        logoContainer.classList.add('logo-container-shrinked');
+        logo.classList.remove('logo');
+        logoContainer.classList.remove('logo-container');
     } else {
         header.style.backgroundColor = 'transparent'; 
+        logo.classList.remove('logo-shrinked');
+        logoContainer.classList.remove('logo-container-shrinked');
+        logo.classList.add('logo');
+        logoContainer.classList.add('logo-container');
+
     }
 });
+
 
 
 document.addEventListener('DOMContentLoaded', displayNextWord);
