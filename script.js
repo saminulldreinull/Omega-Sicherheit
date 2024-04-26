e3d013d7823b07a37771bc721efa0b98bcc4dd5a
saminulldreinull <110830300+saminulldreinull@users.noreply.github.com>
2024-04-26 21:15:48 +0200

    horizontal scrolling is now possible. There is no a need to fix to scroll smooth to section 1 to section to

--- script.js
+++ script.js
@@ -104,3 +104,29 @@ document.addEventListener('DOMContentLoaded', function() {
     animateWords();  // Starte das Text-Schreibeffekt nach dem DOM geladen ist
 });
 
+
+const races = document.querySelector(".races");
+console.log(races.offsetWidth)
+
+function getScrollAmount() {
+	let racesWidth = races.scrollWidth;
+	return -(racesWidth - window.innerWidth);
+}
+
+const tween = gsap.to(races, {
+	x: getScrollAmount,
+	duration: 3,
+	ease: "none",
+});
+
+
+ScrollTrigger.create({
+	trigger:".racesWrapper",
+	start:"top 20%",
+	end: () => `+=${getScrollAmount() * -1}`,
+	pin:true,
+	animation:tween,
+	scrub:1,
+	invalidateOnRefresh:true,
+	markers:false
+})
