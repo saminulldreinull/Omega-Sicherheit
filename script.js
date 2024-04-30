378697c050b3ad0ad1fc232907de5af8c84706f2
saminulldreinull <110830300+saminulldreinull@users.noreply.github.com>
2024-04-30 22:27:21 +0200

    responsive header

--- script.js
+++ script.js
@@ -116,4 +116,40 @@ ScrollTrigger.create({
 	markers:false
 })
 
+window.onload = () => {
+    let openMenu = document.querySelector(".open-menu");
+  
+    openMenu.addEventListener("click", (e) => {
+      document.documentElement.style.overflow = 'hidden';
+    });
+  
+    let closeMenu = document.querySelector(".close-menu");
+    closeMenu.addEventListener("click", (e) => {
+      document.documentElement.style.overflow = 'visible';
+    });
+  
+    let navLinks = document.querySelectorAll(".nav-item");
+    navLinks.forEach((link) => {
+      link.addEventListener("click", (e) => {
+        closeMenu.click(); 
+      });
+    });
+  
+    window.addEventListener("scroll", (event) => {
+      let scroll = this.scrollY;
+      if (scroll > 10) {
+        document.querySelector("header").classList.add("shadow");
+      } else {
+        document.querySelector("header").classList.remove("shadow");
+      }
+    });
+  };
+
+  window.addEventListener('resize', () => {
+    const checkbox = document.getElementById('check');
+    if (window.innerWidth > 1028 && checkbox.checked) {
+      checkbox.checked = false;
+      document.documentElement.style.overflow = 'visible';
+    }
+  });
 
