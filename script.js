c30a7573fd322874b1b53b9f3ef4a8430a6a6f7d
saminulldreinull <110830300+saminulldreinull@users.noreply.github.com>
2024-05-05 15:01:27 +0200

    autoplay landing page video

--- script.js
+++ script.js
@@ -111,6 +111,14 @@ ScrollTrigger.create({
 	markers:false
 })
 
+var ua = navigator.userAgent.toLowerCase();
+var is_safari = (ua.indexOf("safari/") > -1 && ua.indexOf("chrome") < 0);
+if(is_safari) {
+    var video = document.getElementById('#video-element-id');
+    setTimeout(function() {
+       video.play();
+    }, 50);
+}           
 
 
 window.onload = () => {
