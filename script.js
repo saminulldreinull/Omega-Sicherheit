e59187fb90162f5b688701fe38086c9f99ae97d1
saminulldreinull <110830300+saminulldreinull@users.noreply.github.com>
2024-05-05 15:09:35 +0200

    removed safari/chrome javascript for vidoe autoplay

--- script.js
+++ script.js
@@ -111,14 +111,7 @@ ScrollTrigger.create({
 	markers:false
 })
 
-var ua = navigator.userAgent.toLowerCase();
-var is_safari = (ua.indexOf("safari/") > -1 && ua.indexOf("chrome") < 0);
-if(is_safari) {
-    var video = document.getElementById('#video-element-id');
-    setTimeout(function() {
-       video.play();
-    }, 50);
-}           
+
 
 
 window.onload = () => {
