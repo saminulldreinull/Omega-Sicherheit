cf1c00776fd2d2d265134cd1e49991687e239a2f
saminulldreinull <110830300+saminulldreinull@users.noreply.github.com>
2024-05-09 18:06:23 +0200

    responsive Explained-Section

--- Components/JavaScript/textRevealAnimation.js
+++ Components/JavaScript/textRevealAnimation.js
@@ -1,11 +1,13 @@
-gsap.registerPlugin(ScrollTrigger);document.querySelectorAll('.reveal-type').forEach((element) => {
+gsap.registerPlugin(ScrollTrigger);
+
+document.querySelectorAll('.reveal-type').forEach((element) => {
     const originalText = element.innerHTML;
     const bgColor = element.dataset.bgColor; // Hintergrundfarbe aus den Datenattributen
     const fgColor = element.dataset.fgColor; // Vordergrundfarbe aus den Datenattributen
     let isFirstOccurrenceHandled = false;
+
     // Highlight-Funktion für spezielle Wörter
     function highlightText(text) {
-       
         const specialWords = ["Omega Security GmbH", "2021", "Wir", "Beitrag", "Berlins"];
         const firstOccurrenceOnly = "Sicherheit";
 
@@ -29,24 +31,45 @@ gsap.registerPlugin(ScrollTrigger);document.querySelectorAll('.reveal-type').for
     element.innerHTML = originalText.split('<br>').map(line => highlightText(line)).join('<br>');
     const text = new SplitType(element, { types: 'chars' });
 
-    // Animate text color changes on scroll
-    gsap.fromTo(text.chars, {
-        color: bgColor, // Start with the background color
-    }, {
-        color: (i) => {
-            // Anpassen der Farbe basierend auf der Klasse
-            return text.chars[i].parentNode.className.includes('special') ? "rgb(129, 164, 205)" : fgColor;
-        },
-        duration: 0.3,
-        stagger: 0.02,
-        scrollTrigger: {
-            trigger: element,
-            start: 'top 80%',
-            end: 'bottom 55%',
-            scrub: true,
-            markers: false,
-            toggleActions: 'play none none reverse'
+    // Funktion zum Setzen der ScrollTrigger-Einstellungen
+    function setScrollTrigger(start, end) {
+        gsap.fromTo(text.chars, {
+            color: bgColor, // Startfarbe
+        }, {
+            color: (i) => {
+                // Farbanpassung basierend auf Klasse
+                return text.chars[i].parentNode.className.includes('special') ? "rgb(129, 164, 205)" : fgColor;
+            },
+            duration: 0.3,
+            stagger: 0.02,
+            scrollTrigger: {
+                trigger: element,
+                start: start,
+                end: end,
+                scrub: true,
+                markers: false,
+                toggleActions: 'play none none reverse'
+            }
+        });
+    }
+
+    // Media Query beobachten
+    const mq = window.matchMedia('(min-width: 768px)');
+    mq.addEventListener('change', (e) => {
+        // Anpassen der Trigger-Positionen basierend auf der Bildschirmbreite
+        if (e.matches) {
+            // Für größere Bildschirme
+            setScrollTrigger('top 80%', 'bottom 60%');
+        } else {
+            // Für kleinere Bildschirme
+            setScrollTrigger('top 90%', 'bottom 65%');
         }
     });
-});
 
+    // Initialer Aufruf, um die Einstellungen basierend auf der aktuellen Bildschirmgröße zu setzen
+    if (mq.matches) {
+        setScrollTrigger('top 80%', 'bottom 60%');
+    } else {
+        setScrollTrigger('top 90%', 'bottom 65%');
+    }
+});
