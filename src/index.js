ad215a50c505ac80823e4c0f317242d003e51cce
saminulldreinull <110830300+saminulldreinull@users.noreply.github.com>
2024-05-17 12:38:44 +0200

    data base

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ const mongoose = require('mongoose');
 const path = require('path');
 const User = require('./models/user');
 
-// Laden der Umgebungsvariablen aus der .env-Datei
 dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
 console.log('EMAIL:', process.env.EMAIL);
@@ -15,12 +14,10 @@ console.log('EMAIL_PASSWORD:', process.env.EMAIL_PASSWORD);
 const app = express();
 const port = process.env.PORT || 3001;
 
-// Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../public')));
 
-// MongoDB Verbindung
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('Mit MongoDB verbunden');
@@ -29,16 +26,21 @@ mongoose.connect(process.env.MONGO_URI)
     console.error('MongoDB Verbindungsfehler:', err);
   });
 
-// Route für das Senden der E-Mail
 app.post('/send-email', async (req, res) => {
     const { name, email, company, message, privacy } = req.body;
 
-    if (!privacy) {
+    if (privacy !== 'on') {
         return res.status(400).send('Datenschutzerklärung muss akzeptiert werden.');
     }
 
-    // Neuen Nutzer erstellen und speichern
-    const user = new User({ name, email, company, message, privacy });
+    const user = new User({ 
+        name, 
+        email, 
+        company, 
+        message, 
+        privacy: true
+    });
+
     try {
         await user.save();
         console.log('Nutzer erfolgreich gespeichert');
@@ -47,18 +49,16 @@ app.post('/send-email', async (req, res) => {
         return res.status(500).send('Fehler beim Speichern des Nutzers.');
     }
 
-    // Konfigurieren des Transporters für Nodemailer
     const transporter = nodemailer.createTransport({
         host: 'smtp.gmail.com',
         port: 587,
-        secure: false, // true for 465, false for other ports
+        secure: false,
         auth: {
             user: process.env.EMAIL,
             pass: process.env.EMAIL_PASSWORD
         }
     });
 
-    // E-Mail-Optionen definieren (E-Mail an Sie)
     const mailOptionsToYou = {
         from: email,
         to: process.env.EMAIL,
@@ -66,7 +66,6 @@ app.post('/send-email', async (req, res) => {
         text: `Name: ${name}\nEmail: ${email}\nCompany: ${company}\nMessage: ${message}`
     };
 
-    // E-Mail-Optionen definieren (Bestätigung an den Absender)
     const mailOptionsToSender = {
         from: process.env.EMAIL,
         to: email,
@@ -74,7 +73,6 @@ app.post('/send-email', async (req, res) => {
         text: `Hallo ${name},\n\nwir haben Ihre Nachricht erhalten und werden uns in Kürze bei Ihnen melden.\n\nMit freundlichen Grüßen,\nOmega Security GmbH`
     };
 
-    // Senden der E-Mail an Sie
     transporter.sendMail(mailOptionsToYou, (error, info) => {
         if (error) {
             console.error('Fehler beim Senden der E-Mail an Sie:', error);
@@ -82,7 +80,6 @@ app.post('/send-email', async (req, res) => {
         }
         console.log('E-Mail an Sie erfolgreich gesendet:', info.response);
 
-        // Senden der Bestätigungs-E-Mail an den Absender
         transporter.sendMail(mailOptionsToSender, (error, info) => {
             if (error) {
                 console.error('Fehler beim Senden der Bestätigungs-E-Mail:', error);
@@ -94,12 +91,11 @@ app.post('/send-email', async (req, res) => {
     });
 });
 
-// Route, um die index.html Datei zu bedienen
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
-// Route, um alle Benutzer zu holen und zu entschlüsseln
+// Route um alle Nutzer abzurufen und zu entschlüsseln
 app.get('/users', async (req, res) => {
     try {
         const users = await User.find();
@@ -111,23 +107,23 @@ app.get('/users', async (req, res) => {
     }
 });
 
-// Route, um einen spezifischen Benutzer zu holen und zu entschlüsseln
+// Route um einen bestimmten Nutzer abzurufen und zu entschlüsseln
 app.get('/users/:id', async (req, res) => {
     const userId = req.params.id;
     try {
         const user = await User.findById(userId);
-        if (!user) {
-            return res.status(404).send('Benutzer nicht gefunden');
+        if (user) {
+            user.decryptFields();
+            res.status(200).json(user);
+        } else {
+            res.status(404).send('Nutzer nicht gefunden');
         }
-        user.decryptFields();
-        res.status(200).json(user);
     } catch (error) {
         console.error('Fehler beim Abrufen des Nutzers:', error);
         res.status(500).send('Fehler beim Abrufen des Nutzers.');
     }
 });
 
-// Starten des Servers
 app.listen(port, () => {
     console.log(`Server läuft auf Port ${port}`);
 });
--- src/utils/encryption.js
+++ src/utils/encryption.js
@@ -2,9 +2,9 @@ const crypto = require('crypto');
 
 const algorithm = 'aes-256-cbc';
 const key = crypto.createHash('sha256').update(String(process.env.ENCRYPTION_KEY)).digest('base64').substr(0, 32);
-const iv = crypto.randomBytes(16);
 
 function encrypt(text) {
+    const iv = crypto.randomBytes(16);
     let cipher = crypto.createCipheriv(algorithm, Buffer.from(key), iv);
     let encrypted = cipher.update(text);
     encrypted = Buffer.concat([encrypted, cipher.final()]);
