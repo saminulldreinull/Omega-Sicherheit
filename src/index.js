02380c0c0d0550fc8619693b91d07930d2b4e9ee
saminulldreinull <110830300+saminulldreinull@users.noreply.github.com>
2024-05-17 13:32:15 +0200

    https

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const https = require('https');
 const express = require('express');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
@@ -27,103 +29,114 @@ mongoose.connect(process.env.MONGO_URI)
   });
 
 app.post('/send-email', async (req, res) => {
-    const { name, email, company, message, privacy } = req.body;
+  const { name, email, company, message, privacy } = req.body;
 
-    if (privacy !== 'on') {
-        return res.status(400).send('Datenschutzerklärung muss akzeptiert werden.');
+  if (privacy !== 'on') {
+    return res.status(400).send('Datenschutzerklärung muss akzeptiert werden.');
+  }
+
+  const user = new User({
+    name,
+    email,
+    company,
+    message,
+    privacy: true
+  });
+
+  try {
+    await user.save();
+    console.log('Nutzer erfolgreich gespeichert');
+  } catch (error) {
+    console.error('Fehler beim Speichern des Nutzers:', error);
+    return res.status(500).send('Fehler beim Speichern des Nutzers.');
+  }
+
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.EMAIL_PASSWORD
     }
+  });
 
-    const user = new User({ 
-        name, 
-        email, 
-        company, 
-        message, 
-        privacy: true
-    });
+  const mailOptionsToYou = {
+    from: email,
+    to: process.env.EMAIL,
+    subject: `Neue Nachricht von ${name} (${company})`,
+    text: `Name: ${name}\nEmail: ${email}\nCompany: ${company}\nMessage: ${message}`
+  };
 
-    try {
-        await user.save();
-        console.log('Nutzer erfolgreich gespeichert');
-    } catch (error) {
-        console.error('Fehler beim Speichern des Nutzers:', error);
-        return res.status(500).send('Fehler beim Speichern des Nutzers.');
+  const mailOptionsToSender = {
+    from: process.env.EMAIL,
+    to: email,
+    subject: 'Ihre Nachricht an Omega Security GmbH',
+    text: `Hallo ${name},\n\nwir haben Ihre Nachricht erhalten und werden uns in Kürze bei Ihnen melden.\n\nMit freundlichen Grüßen,\nOmega Security GmbH`
+  };
+
+  transporter.sendMail(mailOptionsToYou, (error, info) => {
+    if (error) {
+      console.error('Fehler beim Senden der E-Mail an Sie:', error);
+      return res.status(500).send('Fehler beim Senden der E-Mail.');
     }
+    console.log('E-Mail an Sie erfolgreich gesendet:', info.response);
 
-    const transporter = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 587,
-        secure: false,
-        auth: {
-            user: process.env.EMAIL,
-            pass: process.env.EMAIL_PASSWORD
-        }
+    transporter.sendMail(mailOptionsToSender, (error, info) => {
+      if (error) {
+        console.error('Fehler beim Senden der Bestätigungs-E-Mail:', error);
+        return res.status(500).send('Fehler beim Senden der Bestätigungs-E-Mail.');
+      }
+      console.log('Bestätigungs-E-Mail erfolgreich gesendet:', info.response);
+      res.status(200).send('E-Mail erfolgreich gesendet!');
     });
-
-    const mailOptionsToYou = {
-        from: email,
-        to: process.env.EMAIL,
-        subject: `Neue Nachricht von ${name} (${company})`,
-        text: `Name: ${name}\nEmail: ${email}\nCompany: ${company}\nMessage: ${message}`
-    };
-
-    const mailOptionsToSender = {
-        from: process.env.EMAIL,
-        to: email,
-        subject: 'Ihre Nachricht an Omega Security GmbH',
-        text: `Hallo ${name},\n\nwir haben Ihre Nachricht erhalten und werden uns in Kürze bei Ihnen melden.\n\nMit freundlichen Grüßen,\nOmega Security GmbH`
-    };
-
-    transporter.sendMail(mailOptionsToYou, (error, info) => {
-        if (error) {
-            console.error('Fehler beim Senden der E-Mail an Sie:', error);
-            return res.status(500).send('Fehler beim Senden der E-Mail.');
-        }
-        console.log('E-Mail an Sie erfolgreich gesendet:', info.response);
-
-        transporter.sendMail(mailOptionsToSender, (error, info) => {
-            if (error) {
-                console.error('Fehler beim Senden der Bestätigungs-E-Mail:', error);
-                return res.status(500).send('Fehler beim Senden der Bestätigungs-E-Mail.');
-            }
-            console.log('Bestätigungs-E-Mail erfolgreich gesendet:', info.response);
-            res.status(200).send('E-Mail erfolgreich gesendet!');
-        });
-    });
+  });
 });
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
+  res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 // Route um alle Nutzer abzurufen und zu entschlüsseln
 app.get('/users', async (req, res) => {
-    try {
-        const users = await User.find();
-        users.forEach(user => user.decryptFields());
-        res.status(200).json(users);
-    } catch (error) {
-        console.error('Fehler beim Abrufen der Nutzer:', error);
-        res.status(500).send('Fehler beim Abrufen der Nutzer.');
-    }
+  try {
+    const users = await User.find();
+    users.forEach(user => user.decryptFields());
+    res.status(200).json(users);
+  } catch (error) {
+    console.error('Fehler beim Abrufen der Nutzer:', error);
+    res.status(500).send('Fehler beim Abrufen der Nutzer.');
+  }
 });
 
 // Route um einen bestimmten Nutzer abzurufen und zu entschlüsseln
 app.get('/users/:id', async (req, res) => {
-    const userId = req.params.id;
-    try {
-        const user = await User.findById(userId);
-        if (user) {
-            user.decryptFields();
-            res.status(200).json(user);
-        } else {
-            res.status(404).send('Nutzer nicht gefunden');
-        }
-    } catch (error) {
-        console.error('Fehler beim Abrufen des Nutzers:', error);
-        res.status(500).send('Fehler beim Abrufen des Nutzers.');
+  const userId = req.params.id;
+  try {
+    const user = await User.findById(userId);
+    if (user) {
+      user.decryptFields();
+      res.status(200).json(user);
+    } else {
+      res.status(404).send('Nutzer nicht gefunden');
     }
+  } catch (error) {
+    console.error('Fehler beim Abrufen des Nutzers:', error);
+    res.status(500).send('Fehler beim Abrufen des Nutzers.');
+  }
 });
 
 app.listen(port, () => {
-    console.log(`Server läuft auf Port ${port}`);
+  console.log(`HTTP Server läuft auf Port ${port}`);
 });
+
+// Laden Sie die Zertifikatsdateien
+const options = {
+  key: fs.readFileSync(path.join(__dirname, '../server.key')),
+  cert: fs.readFileSync(path.join(__dirname, '../server.cert'))
+};
+
+// HTTPS-Server erstellen
+https.createServer(options, app).listen(443, () => {
+    console.log(`HTTPS Server läuft auf http://localhost:${port}`);
+  });
