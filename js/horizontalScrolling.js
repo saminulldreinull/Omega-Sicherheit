438d56acfe2733b041a5046f2b9743717df98179
saminulldreinull <110830300+saminulldreinull@users.noreply.github.com>
2024-05-20 00:13:40 +0200

    Updates

--- js/horizontalScrolling.js
+++ js/horizontalScrolling.js
@@ -1,24 +1,52 @@
-
 const races = document.querySelector(".races");
+
 function getScrollAmount() {
-	let racesWidth = races.scrollWidth;
-	return -(racesWidth - window.innerWidth);
+  let racesWidth = races.scrollWidth;
+  return -(racesWidth - window.innerWidth);
 }
 
-const tween = gsap.to(races, {
+let tween, scrollTriggerInstance;
+
+function createScrollAnimation() {
+  tween = gsap.to(races, {
     x: () => getScrollAmount(),
     duration: 3,
     ease: "none",
     force3D: true
-});
+  });
 
-ScrollTrigger.create({
+  scrollTriggerInstance = ScrollTrigger.create({
     trigger: ".racesWrapper",
     start: "top 20%",
     end: () => `+=${document.querySelector(".races").scrollWidth}`,
-    pin: true, 
+    pin: true,
     animation: tween,
     scrub: true,
-    invalidateOnRefresh: true,
-    onRefresh: () => tween.invalidate()
-});
+    invalidateOnRefresh: true
+  });
+
+  console.log('ScrollTrigger instance created.');
+}
+
+function refreshServiceSection() {
+  console.log('Resizing window, refreshing service section...');
+
+  if (tween) {
+    tween.kill();  // Kill the previous tween
+  }
+
+  if (scrollTriggerInstance) {
+    scrollTriggerInstance.kill();  // Kill the previous ScrollTrigger instance
+  }
+
+  // Recreate the animation and ScrollTrigger instance
+  createScrollAnimation();
+
+  // Refresh ScrollTrigger calculations
+  ScrollTrigger.refresh();
+
+  console.log('ScrollTrigger refreshed.');
+}
+
+// Initial creation of scroll animation
+createScrollAnimation();
