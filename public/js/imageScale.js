a5ac27ced7171cad2357c4ba3b8a83ca3e195f70
saminulldreinull <110830300+saminulldreinull@users.noreply.github.com>
2024-05-25 13:44:52 +0200

    footer

--- public/js/imageScale.js
+++ public/js/imageScale.js
@@ -1,65 +1,41 @@
-const imageElement = document.getElementById("contact-image");
-
-function makeSticky() {
-  imageElement.classList.add('sticky');
-}
-
-function removeSticky() {
-  imageElement.classList.remove('sticky');
-}
-
-function checkPosition() {
-  const contactSection = document.getElementById("contact-section");
-  const rect = contactSection.getBoundingClientRect();
-  
-  if (rect.top <= 0 && rect.bottom >= 0) {
-    makeSticky();
-  } else {
-    removeSticky();
-  }
-}
-
-function initAnimation() {
-  const tl = gsap.timeline({
-    ease: "none",
-    onUpdate: function() {
-      if (tl.progress() > 0.5) {
-        imageElement.style.borderRadius = '0';
-      } else {
-        imageElement.style.borderRadius = '15px';
-      }
-    }
-  });
-
-  tl.from(imageElement, {
-    scale: 0.9,
-    duration: 1,
-    transformOrigin: "center",
-  }).to({}, {
-    duration: 1
-  });
-
-  ScrollTrigger.create({
-    trigger: "#contact-section",
-    start: "top top",
-    end: "bottom top",
-    animation: tl,
-    scrub: 0.8,
-    pin: imageElement,
-    pinSpacing: false,
-    onEnter: () => {
-      makeSticky();
-    },
-    onLeaveBack: () => {
-      removeSticky();
-    }
-  });
-}
-
 document.addEventListener('DOMContentLoaded', () => {
+  const imageElement = document.getElementById("contact-image");
+  const contactSection = document.getElementById("contact-section");
+
+  function makeSticky() {
+    imageElement.classList.add('sticky');
+  }
+
+  function removeSticky() {
+    imageElement.classList.remove('sticky');
+  }
+
+  function initAnimation() {
+    ScrollTrigger.create({
+      trigger: "#contact-section",
+      start: "top top",
+      end: "bottom top",
+      pin: imageElement,
+      pinSpacing: false,
+      onUpdate: self => {
+        const progress = self.progress;
+        imageElement.style.clipPath = `inset(0 0 ${progress * 100}% 0)`;
+      },
+      onEnter: () => makeSticky(),
+      onLeave: () => removeSticky(),
+      onLeaveBack: () => makeSticky(),
+    });
+  }
+
   if (window.matchMedia("(max-width: 768px)").matches) {
-    checkPosition();
-    window.addEventListener('scroll', checkPosition);
+    window.addEventListener('scroll', () => {
+      const rect = contactSection.getBoundingClientRect();
+      if (rect.top <= 0 && rect.bottom >= 0) {
+        makeSticky();
+      } else {
+        removeSticky();
+      }
+    });
   } else {
     initAnimation();
   }
