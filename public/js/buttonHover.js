fd4258b358e57214c4878f6857cbcbf9d2e9d821
saminulldreinull <110830300+saminulldreinull@users.noreply.github.com>
2024-07-24 14:02:09 +0200

    button hover effect on form button

--- public/js/buttonHover.js
+++ public/js/buttonHover.js
@@ -4,15 +4,15 @@ document.querySelectorAll('.button-container, .form-button-container, .float',).
       const x = e.clientX - rect.left - rect.width / 2;
       const y = e.clientY - rect.top - rect.height / 2;
       gsap.to(button, {
-          x: x * 0.2, // Stärkere Bewegung
+          x: x * 0.2,
           y: y * 0.2,
-          scale: 1.05, // Leichte Skalierung auf 105% der Originalgröße
+          scale: 1.05,
           duration: 0.3,
           ease: 'power1.out'
       });
   });
 
   button.addEventListener('mouseleave', () => {
-      gsap.to(button, {x: 0, y: 0, scale: 1, duration: 0.3, ease: 'power1.out'}); // Rücksetzung auf normale Größe und Position
+      gsap.to(button, {x: 0, y: 0, scale: 1, duration: 0.3, ease: 'power1.out'});
   });
 });
