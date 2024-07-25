c4bd39055b7f40acb6d7f402171b448e8c8f8998
saminulldreinull <110830300+saminulldreinull@users.noreply.github.com>
2024-07-25 12:38:06 +0200

    Updates

--- js/contactForm.js
+++ js/contactForm.js
@@ -1,22 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
-  let contactForm = document.getElementById('contactForm');
+  var contactForm = document.getElementById('contactForm');
   if (contactForm) {
     contactForm.addEventListener('submit', function(event) {
       event.preventDefault();
 
-      let privacyCheckbox = document.getElementById('privacy');
+      var privacyCheckbox = document.getElementById('privacy');
       if (!privacyCheckbox.checked) {
         alert('Bitte akzeptieren Sie die Datenschutzerklärung.');
         return;
       }
 
-      let formData = new FormData(this);
-      let jsonObject = {};
+      var formData = new FormData(this);
+      var jsonObject = {};
       formData.forEach((value, key) => {
         jsonObject[key] = value;
       });
-
-      console.log(jsonObject); // Debugging
 
       fetch('/contact/send-email', {
         method: 'POST',
@@ -27,12 +25,10 @@ document.addEventListener('DOMContentLoaded', function() {
       })
       .then(response => response.text())
       .then(data => {
-        console.log('Response Text:', data); // Debugging
         if (data.includes('E-Mail erfolgreich gesendet!')) {
           document.getElementById('alert-success').style.display = 'flex';
           document.getElementById('alert-danger').style.display = 'none';
           document.getElementById('alert-container').style.display = 'block';
-          console.log('Success Alert Triggered');
 
           // Hide success alert after 5 seconds
           setTimeout(() => {
@@ -46,11 +42,9 @@ document.addEventListener('DOMContentLoaded', function() {
         }
       })
       .catch(error => {
-        console.error('Fetch error:', error);
         document.getElementById('alert-success').style.display = 'none';
         document.getElementById('alert-danger').style.display = 'flex';
         document.getElementById('alert-container').style.display = 'block';
-        console.log('Error Alert Triggered');
 
         // Hide error alert after 5 seconds
         setTimeout(() => {
@@ -66,7 +60,5 @@ document.addEventListener('DOMContentLoaded', function() {
     document.querySelectorAll('.close').forEach(function(closeBtn) {
       closeBtn.addEventListener('click', closeAlert);
     });
-  } else {
-    console.error('Contact form not found');
   }
 });
