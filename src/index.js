30700918099e8c2579b9cf33f35c315007791ae3
saminulldreinull <110830300+saminulldreinull@users.noreply.github.com>
2024-09-04 23:54:53 +0200

    hope

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ app.use(helmet());
 const corsOptions = {
     origin: ['https://omega-sicherheit.com', 'https://www.omega-sicherheit.com', 'https://backend.omega-sicherheit.com'],
     methods: ['GET', 'POST', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'CSRF-Token', 'Authorization', 'X-Requested-With'],
+    allowedHeaders: ['Content-Type', 'CSRF-Token', 'Authorization'],
     credentials: true,
 };
 
@@ -31,14 +31,14 @@ app.options('*', cors(corsOptions));
 // Cookie Parser verwenden, um CSRF-Token in Cookies zu speichern
 app.use(cookieParser());
 
+// CSRF-Token-Route definieren - diese Route sollte nicht durch die CSRF-Middleware geschützt sein
+app.get('/get-csrf-token', (req, res) => {
+  res.json({ csrfToken: req.csrfToken() });
+});
+
 // Middleware für JSON-Parsing
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-// **CSRF-Token-Route definieren** - Diese Route sollte **vor** der CSRF-Middleware sein
-app.get('/get-csrf-token', (req, res) => {
-    res.json({ csrfToken: req.csrfToken() });
-});
 
 // CSRF-Middleware hinzufügen - NUR für POST- oder geschützte Routen
 app.use(csurf({ cookie: true }));
@@ -48,9 +48,9 @@ app.use(morgan('combined'));
 
 // Rate Limiting für Anmelde- und Registrierungsrouten
 const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 Minuten
-    max: 100, // Beschränkung auf 100 Anfragen pro IP und Fenster
-    message: 'Zu viele Anfragen von dieser IP, bitte versuchen Sie es später erneut.',
+  windowMs: 15 * 60 * 1000, // 15 Minuten
+  max: 100, // Beschränkung auf 100 Anfragen pro IP und Fenster
+  message: 'Zu viele Anfragen von dieser IP, bitte versuchen Sie es später erneut.',
 });
 app.use('/admin/login', limiter);
 app.use('/admin/register', limiter);
@@ -60,7 +60,7 @@ app.use(express.static(path.join(__dirname, '..', 'public')));
 
 // Root-Route hinzufügen
 app.get('/', (req, res) => {
-    res.send('Willkommen auf dem Omega-Sicherheits-Backend!');
+  res.send('Willkommen auf dem Omega-Sicherheits-Backend!');
 });
 
 // Beispielrouten
@@ -70,11 +70,11 @@ app.use('/contact', contactRoutes);
 
 // Fehlerbehandlung für CSRF-Fehler
 app.use((err, req, res, next) => {
-    if (err.code === 'EBADCSRFTOKEN') {
-        res.status(403).send('Ungültiger CSRF-Token');
-    } else {
-        next(err);
-    }
+  if (err.code === 'EBADCSRFTOKEN') {
+    res.status(403).send('Ungültiger CSRF-Token');
+  } else {
+    next(err);
+  }
 });
 
 // HTTP-Server starten
@@ -82,5 +82,5 @@ const PORT = process.env.PORT || 8081;
 const HOST = '0.0.0.0';
 
 app.listen(PORT, HOST, () => {
-    console.log(`Server läuft auf http://${HOST}:${PORT}`);
+  console.log(`Server läuft auf http://${HOST}:${PORT}`);
 });
